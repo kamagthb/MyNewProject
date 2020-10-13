8cd090d426cfa32b26bdd3bac35354dde36caef7
kamagthb <esrefkaya2020@gmail.com>
2020-10-13 23:27:55 +0300

    3.  com

--- src/test/java/pac1/class1.java
+++ src/test/java/pac1/class1.java
@@ -4,5 +4,6 @@ public class class1 {
     public static void main(String[] args) {
         System.out.println("good");
         System.out.println("new");
+        System.out.println("new");
     }
 }
